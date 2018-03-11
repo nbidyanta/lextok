c2d1dc51b3c071b4a053e30097c5448c5cd7aa67
nbidyanta <nbidyanta@users.noreply.github.com>
2018-03-11 03:02:09 -0400

    Completed designing first test suite:
    Tests for the alphabet tokenizer were written;
    Added convenience equality operator for CT::string_view;

--- tests/common.h
+++ tests/common.h
@@ -2,15 +2,31 @@
 #define _COMMON_H
 
 #include <iostream>
+#include <algorithm>
 #include "lextok.h"
 
 using std::cout;
 
-std::ostream& operator<<(std::ostream& out, const CT::string_view& sv)
-{
-  for (const auto& c : sv)
-    out << c;
-  return out;
+namespace CT {
+
+  std::ostream& operator<<(std::ostream& out, const CT::string_view& sv)
+  {
+    for (const auto& c : sv)
+      out << c;
+    return out;
+  }
+
+  bool operator==(const string_view& lhs, const string_view& rhs)
+  {
+    if (lhs.size() != rhs.size())
+      return false;
+    if (std::mismatch(lhs.begin(), lhs.end(),
+          rhs.begin(), rhs.end()) == std::make_pair(lhs.end(), rhs.end()))
+      return true;
+    return false;
+  }
+
 }
+
 
 #endif
--- tests/test_alphabet_match.cpp
+++ tests/test_alphabet_match.cpp
@@ -1,23 +1,76 @@
 #include "common.h"
+#include <functional>
 
-constexpr CT::string_view i0{"aBCd12434"};
+static CT::string_view input[] = {
+  {},                           // Empty input
+  {"12-4"},                     // No matches
+  {"G"},                        // Normal match (upper case)
+  {"x"},                        // Normal match (lower case)
+  {"nB"},                       // Multiple matches (non-zero)
+  {"00"},                       // Multiple matches (zero)
+  {"f1"},                       // At least one match (positive)
+  {"19"},                       // At least one match (negative)
+  {"ThisIsACamelCaseSentence"}  // Count number of tokens matched
+};
 
-constexpr auto lex(const CT::string_view& sv)
-{
-  std::size_t sz = 0;
-  auto i = sv;
-  const auto letters = Tok::many(Tok::alphabet([&sz](CT::string_view token){ sz++; }));
-  const auto token = letters(i);
-  return token;
-}
+static constexpr std::size_t number_of_tests = sizeof(input) / sizeof(CT::string_view);
+
+static std::function<bool(CT::string_view& input)> test_drivers[number_of_tests] = {
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::alphabet()(input);
+    return !token ? true : false;
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::alphabet()(input);
+    return !token ? true : false;
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::alphabet()(input);
+    return token && *token == "G";
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::alphabet()(input);
+    return token && *token == "x";
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::many(Tok::alphabet())(input);
+    return token && *token == "nB";
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::many(Tok::alphabet())(input);
+    return token && *token == "";
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::at_least_one(Tok::alphabet())(input);
+    return token && *token == "f";
+  },
+
+  [](CT::string_view& input) -> bool {
+    const auto token = Tok::at_least_one(Tok::alphabet())(input);
+    return !token ? true : false;
+  },
+
+  [](CT::string_view& input) -> bool {
+    std::size_t sz = 0;
+    const auto token = Tok::at_least_one(Tok::alphabet([&sz](CT::string_view){ sz++; }))(input);
+    return token && *token == "ThisIsACamelCaseSentence" && sz == CT::strlen("ThisIsACamelCaseSentence");
+  }
+
+};
 
 int main()
 {
-  constexpr auto token = lex(i0);
-  if (!token) {
-    cout << "Not found\n";
-    return 1;
-  }
-  cout << "Found : " << (*token) << "\n";
+  for (std::size_t i = 0; i < number_of_tests; i++)
+    if (!test_drivers[i](input[i])) {
+      std::cerr << "Subtest " << i << " has failed\n";
+      return 1;
+    }
   return 0;
 }
